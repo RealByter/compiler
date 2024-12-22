980300f5f9c6bb94c04da3c1843f872354235b2a
RealByter <justavishay@gmail.com>
2024-12-22 09:11:04 +0200

    complete the code generation for logical operators

--- return_2.s
+++ return_2.s
@@ -2,7 +2,7 @@
 main:
 	pushq %rbp
 	movq %rsp, %rbp
-	subq $24, %rsp
+	subq $68, %rsp
 	movl $3, -4(%rbp)
 	movl -4(%rbp), %r11d
 	imull $4, %r11d
@@ -20,13 +20,58 @@ main:
 	movl %eax, -16(%rbp)
 	movl -16(%rbp), %eax
 	cdq
-	movl $6, %r10d
+	movl $7, %r10d
 	idivl %r10d
 	movl %edx, -20(%rbp)
-	movl -20(%rbp), %r10d
-	movl %r10d, -24(%rbp)
-	notl -24(%rbp)
-	movl -24(%rbp), %eax
+	cmpl $0, -20(%rbp)
+	movl $0, -24(%rbp)
+	sete -24(%rbp)
+	cmpl $0, -24(%rbp)
+	movl $0, -28(%rbp)
+	sete -28(%rbp)
+	cmpl $1, -28(%rbp)
+	movl $0, -32(%rbp)
+	setne -32(%rbp)
+	cmpl $0, -32(%rbp)
+	je .Llabel_false.2
+	movl $3, -36(%rbp)
+	movl -36(%rbp), %r11d
+	imull $4, %r11d
+	movl %r11d, -36(%rbp)
+	movl $2, -40(%rbp)
+	movl -36(%rbp), %r10d
+	addl %r10d, -40(%rbp)
+	cmpl $10, -40(%rbp)
+	movl $0, -44(%rbp)
+	setg -44(%rbp)
+	cmpl $0, -44(%rbp)
+	je .Llabel_false.2
+	movl $1, -48(%rbp)
+	jmp .Llabel_and_end.3
+.Llabel_false.2:
+	movl $0, -48(%rbp)
+.Llabel_and_end.3:
+	cmpl $0, -48(%rbp)
+	jne .Llabel_true.0
+	movl $5, -52(%rbp)
+	subl $3, -52(%rbp)
+	cmpl $1, -52(%rbp)
+	movl $0, -56(%rbp)
+	setle -56(%rbp)
+	cmpl $0, -56(%rbp)
+	jne .Llabel_true.0
+	movl $0, -60(%rbp)
+	jmp .Llabel_or_end.1
+.Llabel_true.0:
+	movl $1, -60(%rbp)
+.Llabel_or_end.1:
+	movl -60(%rbp), %r10d
+	movl %r10d, -64(%rbp)
+	notl -64(%rbp)
+	movl -64(%rbp), %r10d
+	movl %r10d, -68(%rbp)
+	notl -68(%rbp)
+	movl -68(%rbp), %eax
 	movq %rbp, %rsp
 	popq %rbp
 	ret
--- src/generator.rs
+++ src/generator.rs
@@ -1,139 +1,192 @@
-// use crate::assembler;
-// use std::fs::File;
-// use std::io::{self, Write};
+use crate::assembler;
+use std::fs::File;
+use std::io::{self, Write};
 
-// fn write_reg(file: &mut File, reg: assembler::Reg) -> io::Result<()> {
-//     match reg {
-//         assembler::Reg::AX => write!(file, "%eax")?,
-//         assembler::Reg::DX => write!(file, "%edx")?,
-//         assembler::Reg::R10 => write!(file, "%r10d")?,
-//         assembler::Reg::R11 => write!(file, "%r11d")?,
-//     }
-//     Ok(())
-// }
+fn write_reg(file: &mut File, reg: assembler::Reg) -> io::Result<()> {
+    match reg {
+        assembler::Reg::AX => write!(file, "%eax")?,
+        assembler::Reg::DX => write!(file, "%edx")?,
+        assembler::Reg::R10 => write!(file, "%r10d")?,
+        assembler::Reg::R11 => write!(file, "%r11d")?,
+    }
+    Ok(())
+}
 
-// fn write_stack(file: &mut File, offset: i64) -> io::Result<()> {
-//     write!(file, "{}(%rbp)", offset)
-// }
+fn write_reg_byte(file: &mut File, reg: assembler::Reg) -> io::Result<()> {
+    match reg {
+        assembler::Reg::AX => write!(file, "%al")?,
+        assembler::Reg::DX => write!(file, "%dl")?,
+        assembler::Reg::R10 => write!(file, "%r10b")?,
+        assembler::Reg::R11 => write!(file, "%r11b")?,
+    }
+    Ok(())
+}
 
-// fn write_imm(file: &mut File, value: i64) -> io::Result<()> {
-//     write!(file, "${}", value)
-// }
+fn write_stack(file: &mut File, offset: i64) -> io::Result<()> {
+    write!(file, "{}(%rbp)", offset)
+}
 
-// fn write_operand(file: &mut File, operand: assembler::Operand) -> io::Result<()> {
-//     match operand {
-//         assembler::Operand::Imm(value) => write_imm(file, value)?,
-//         assembler::Operand::Reg(reg) => write_reg(file, reg)?,
-//         assembler::Operand::Stack(offset) => write_stack(file, offset)?,
-//         assembler::Operand::Pseudo(_) => panic!("Shouldn't have a pseudo register at this stage."),
-//     }
-//     Ok(())
-// }
+fn write_imm(file: &mut File, value: i64) -> io::Result<()> {
+    write!(file, "${}", value)
+}
 
-// fn write_operand_not_imm(file: &mut File, operand: assembler::Operand) -> io::Result<()> {
-//     match operand {
-//         assembler::Operand::Imm(_) => {
-//             panic!("Invalid operand imm.");
-//         }
-//         assembler::Operand::Reg(reg) => write_reg(file, reg)?,
-//         assembler::Operand::Stack(offset) => write_stack(file, offset)?,
-//         assembler::Operand::Pseudo(_) => {
-//             panic!("Shouldn't have a pseudo register at this stage.");
-//         }
-//     }
-//     Ok(())
-// }
+fn write_operand(file: &mut File, operand: assembler::Operand) -> io::Result<()> {
+    match operand {
+        assembler::Operand::Imm(value) => write_imm(file, value)?,
+        assembler::Operand::Reg(reg) => write_reg(file, reg)?,
+        assembler::Operand::Stack(offset) => write_stack(file, offset)?,
+        assembler::Operand::Pseudo(_) => panic!("Shouldn't have a pseudo register at this stage."),
+    }
+    Ok(())
+}
 
-// fn seperate(file: &mut File) -> io::Result<()> {
-//     write!(file, ", ")
-// }
+fn write_operand_byte(file: &mut File, operand: assembler::Operand) -> io::Result<()> {
+    match operand {
+        assembler::Operand::Imm(value) => write_imm(file, value)?,
+        assembler::Operand::Reg(reg) => write_reg_byte(file, reg)?,
+        assembler::Operand::Stack(offset) => write_stack(file, offset)?,
+        assembler::Operand::Pseudo(_) => panic!("Shouldn't have a pseudo register at this stage."),
+    }
+    Ok(())
+}
 
-// fn newline(file: &mut File) -> io::Result<()> {
-//     writeln!(file)
-// }
+fn write_operand_not_imm(file: &mut File, operand: assembler::Operand) -> io::Result<()> {
+    match operand {
+        assembler::Operand::Imm(_) => {
+            panic!("Invalid operand imm.");
+        }
+        assembler::Operand::Reg(reg) => write_reg(file, reg)?,
+        assembler::Operand::Stack(offset) => write_stack(file, offset)?,
+        assembler::Operand::Pseudo(_) => {
+            panic!("Shouldn't have a pseudo register at this stage.");
+        }
+    }
+    Ok(())
+}
 
-// pub fn generate(file_path: &str, program: assembler::Program) -> io::Result<()> {
-//     let mut file = File::create(file_path)?;
+fn seperate(file: &mut File) -> io::Result<()> {
+    write!(file, ", ")
+}
 
-//     writeln!(file, "\t.globl {}", program.function.name)?;
-//     writeln!(file, "{}:", program.function.name)?;
-//     writeln!(file, "\tpushq %rbp")?;
-//     writeln!(file, "\tmovq %rsp, %rbp")?;
-//     for instruction in program.function.instructions {
-//         match instruction {
-//             assembler::Instruction::AllocateStack(size) => {
-//                 writeln!(file, "\tsubq ${}, %rsp", size)?;
-//             }
-//             assembler::Instruction::Mov(src, dst) => {
-//                 write!(file, "\tmovl ")?;
-//                 write_operand(&mut file, src)?;
-//                 seperate(&mut file)?;
-//                 write_operand_not_imm(&mut file, dst)?;
-//                 newline(&mut file)?;
-//             }
-//             assembler::Instruction::Unary(operator, operand) => {
-//                 match operator {
-//                     assembler::UnaryOperator::Neg => {
-//                         write!(file, "\tnegl ")?;
-//                     }
-//                     assembler::UnaryOperator::Not => {
-//                         write!(file, "\tnotl ")?;
-//                     }
-//                 }
+fn newline(file: &mut File) -> io::Result<()> {
+    writeln!(file)
+}
 
-//                 write_operand_not_imm(&mut file, operand)?;
-//                 newline(&mut file)?;
-//             }
-//             assembler::Instruction::Binary(operator, operand1, operand2) => {
-//                 match operator {
-//                     assembler::BinaryOperator::Add => {
-//                         write!(file, "\taddl ")?;
-//                     }
-//                     assembler::BinaryOperator::Sub => {
-//                         write!(file, "\tsubl ")?;
-//                     }
-//                     assembler::BinaryOperator::Mult => {
-//                         write!(file, "\timull ")?;
-//                     }
-//                     assembler::BinaryOperator::And => {
-//                         write!(file, "\tandl ")?;
-//                     }
-//                     assembler::BinaryOperator::Or => {
-//                         write!(file, "\torl ")?;
-//                     }
-//                     assembler::BinaryOperator::Xor => {
-//                         write!(file, "\txorl ")?;
-//                     }
-//                     assembler::BinaryOperator::LeftShift => {
-//                         write!(file, "\tshl ")?;
-//                     }
-//                     assembler::BinaryOperator::RightShift => {
-//                         write!(file, "\tshr ")?;
-//                     }
-//                 }
+// FIRST LIFETIME USAGE
+fn cond_code_to_letters<'a>(cc: assembler::CondCode) -> &'a str {
+    match cc {
+        assembler::CondCode::E => "e",
+        assembler::CondCode::NE => "ne",
+        assembler::CondCode::L => "l",
+        assembler::CondCode::LE => "le",
+        assembler::CondCode::G => "g",
+        assembler::CondCode::GE => "ge",
+    }
+}
 
-//                 write_operand(&mut file, operand1)?;
-//                 seperate(&mut file)?;
-//                 write_operand_not_imm(&mut file, operand2)?;
-//                 newline(&mut file)?;
-//             }
-//             assembler::Instruction::Idiv(operand) => {
-//                 write!(file, "\tidivl ")?;
-//                 write_operand_not_imm(&mut file, operand)?;
-//                 newline(&mut file)?;
-//             }
-//             assembler::Instruction::Cdq => {
-//                 writeln!(file, "\tcdq")?;
-//             }
-//             assembler::Instruction::Ret => {
-//                 writeln!(file, "\tmovq %rbp, %rsp")?;
-//                 writeln!(file, "\tpopq %rbp")?;
-//                 writeln!(file, "\tret")?;
-//             }
-//         }
-//     }
+pub fn generate(file_path: &str, program: assembler::Program) -> io::Result<()> {
+    let mut file = File::create(file_path)?;
 
-//     writeln!(file, ".section .note.GNU-stack,\"\",@progbits")?;
+    writeln!(file, "\t.globl {}", program.function.name)?;
+    writeln!(file, "{}:", program.function.name)?;
+    writeln!(file, "\tpushq %rbp")?;
+    writeln!(file, "\tmovq %rsp, %rbp")?;
+    for instruction in program.function.instructions {
+        match instruction {
+            assembler::Instruction::AllocateStack(size) => {
+                writeln!(file, "\tsubq ${}, %rsp", size)?;
+            }
+            assembler::Instruction::Mov(src, dst) => {
+                write!(file, "\tmovl ")?;
+                write_operand(&mut file, src)?;
+                seperate(&mut file)?;
+                write_operand_not_imm(&mut file, dst)?;
+                newline(&mut file)?;
+            }
+            assembler::Instruction::Unary(operator, operand) => {
+                match operator {
+                    assembler::UnaryOperator::Neg => {
+                        write!(file, "\tnegl ")?;
+                    }
+                    assembler::UnaryOperator::Not => {
+                        write!(file, "\tnotl ")?;
+                    }
+                }
 
-//     Ok(())
-// }
+                write_operand_not_imm(&mut file, operand)?;
+                newline(&mut file)?;
+            }
+            assembler::Instruction::Binary(operator, operand1, operand2) => {
+                match operator {
+                    assembler::BinaryOperator::Add => {
+                        write!(file, "\taddl ")?;
+                    }
+                    assembler::BinaryOperator::Sub => {
+                        write!(file, "\tsubl ")?;
+                    }
+                    assembler::BinaryOperator::Mult => {
+                        write!(file, "\timull ")?;
+                    }
+                    assembler::BinaryOperator::And => {
+                        write!(file, "\tandl ")?;
+                    }
+                    assembler::BinaryOperator::Or => {
+                        write!(file, "\torl ")?;
+                    }
+                    assembler::BinaryOperator::Xor => {
+                        write!(file, "\txorl ")?;
+                    }
+                    assembler::BinaryOperator::LeftShift => {
+                        write!(file, "\tshl ")?;
+                    }
+                    assembler::BinaryOperator::RightShift => {
+                        write!(file, "\tshr ")?;
+                    }
+                }
+
+                write_operand(&mut file, operand1)?;
+                seperate(&mut file)?;
+                write_operand_not_imm(&mut file, operand2)?;
+                newline(&mut file)?;
+            }
+            assembler::Instruction::Idiv(operand) => {
+                write!(file, "\tidivl ")?;
+                write_operand_not_imm(&mut file, operand)?;
+                newline(&mut file)?;
+            }
+            assembler::Instruction::Cdq => {
+                writeln!(file, "\tcdq")?;
+            }
+            assembler::Instruction::Cmp(operand1, operand2) => {
+                write!(file, "\tcmpl ")?;
+                write_operand(&mut file, operand1)?;
+                seperate(&mut file)?;
+                write_operand_not_imm(&mut file, operand2)?;
+                newline(&mut file)?;
+            }
+            assembler::Instruction::Jmp(target) => {
+                writeln!(file, "\tjmp .L{}", target)?;
+            }
+            assembler::Instruction::JmpCC(cc, target) => {
+                writeln!(file, "\tj{} .L{}", cond_code_to_letters(cc), target)?;
+            }
+            assembler::Instruction::SetCC(cc, dst) => {
+                write!(file, "\tset{} ", cond_code_to_letters(cc))?;
+                write_operand_byte(&mut file, dst)?;
+                newline(&mut file)?;
+            }
+            assembler::Instruction::Label(identifier) => {
+                writeln!(file, ".L{}:", identifier)?;
+            }
+            assembler::Instruction::Ret => {
+                writeln!(file, "\tmovq %rbp, %rsp")?;
+                writeln!(file, "\tpopq %rbp")?;
+                writeln!(file, "\tret")?;
+            }
+        }
+    }
+
+    writeln!(file, ".section .note.GNU-stack,\"\",@progbits")?;
+
+    Ok(())
+}
